2237a05bae628fabb2cce7af03d80ea752bad597
Paul Gichuki <pherndegz@gmail.com>
2021-08-11 18:26:28 +0300

    Add footer to Landing page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Search from './components/Search';
 import Landing from './components/Landing';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Fragment>
             <Landing />
             <Search />
+            <Footer />
     </Fragment>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return ( 
+        <div className="max-w-2xl mx-auto text-center my-2">
+            <footer>&copy; Copyright 2021 Raul</footer>
+            <span>Images from <a className="text-blue-600" href="https://placekitten.com/"> PlaceKittens </a></span>
+        </div>
+        );
+}
+ 
+export default Footer;
